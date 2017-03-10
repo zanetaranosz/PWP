1215a41364b446d3c2fadef0ae06ad5a793f12b8
Student <student@ib.polsl.pl>
2017-03-10 14:07:55 +0100

    Zbudowanie projektu bez uzycia Trygonometry degree

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,16 @@
+#include "LaborkaConfig.h"
+#ifdef USE_TRYGONOMETRY_DEGREE
+	#include<trygonometria.h>
+#else
+	#define USE_MATH_DEFINES
+	#include<math.h>
+#endif
+
 int main() {
+	double result; 
+#ifdef USE_TRYGONOMETRY_DEGREE
+	result = cos_degree(45.0);
+#else
+	result = cos(M_PI/4.0);
+#endif
 return 0;}
